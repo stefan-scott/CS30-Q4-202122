3ea902e9eb52462c30aa677cb3df0442f8b634c5
stefan-scott <scottst@spsd.sk.ca>
2022-05-06 14:48:16 -0600

    updated demo

--- 03 Pickupable Primitives/sketch.js
+++ 03 Pickupable Primitives/sketch.js
@@ -11,19 +11,37 @@ let rLeft, rRight, rTop, rBottom;
 function setup() {
   createCanvas(windowWidth, windowHeight);
   rectMode(CENTER);
-  x = width/2;  y = height/2;
-  rWidth = 200;  rHeight = 100;
+  x = width / 2; y = height / 2;
+  rWidth = 200; rHeight = 100;
 }
 
 function draw() {
   background(220);
+  updateEdgePositions();
   drawRectangle();
+
+  print(x + " " + y + " " + rLeft + " " + rRight + " " + rTop + " " + rBottom);
 }
 
-function drawRectangle(){
+function drawRectangle() {
   //render the rectangle and also checks for mouse interactions
+  if (mouseX < rRight && mouseX > rLeft && mouseY > rTop && mouseY < rBottom) {
+    fill(220,10,255);  
+  }
+  else{
+    fill(255);
+  }
+
 
   rect(x, y, rWidth, rHeight);
 
+}
+
+function updateEdgePositions() {
+  //update the left/right/top/bottom variables for our rectangle;
+  rLeft = x - rWidth / 2;
+  rRight = x + rWidth / 2;
+  rTop = y - rHeight / 2;
+  rBottom = y + rHeight / 2;
 }
 
