70034a010b15f0cc7409afc5e098321c867d0997
stefan-scott <scottst@spsd.sk.ca>
2022-05-06 15:05:48 -0600

    Finished Pickupable Primitives

--- 03 Pickupable Primitives/sketch.js
+++ 03 Pickupable Primitives/sketch.js
@@ -6,7 +6,8 @@
 //Global Variable Declarations
 let x, y, rWidth, rHeight;
 let rLeft, rRight, rTop, rBottom;
-
+let mouseOver = false;  let pickedUp = false;
+let xOff, yOff;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -20,21 +21,25 @@ function draw() {
   updateEdgePositions();
   drawRectangle();
 
-  print(x + " " + y + " " + rLeft + " " + rRight + " " + rTop + " " + rBottom);
+  print(mouseOver + " " + pickedUp + " " + x + " " + y + " " + rLeft + " " + rRight + " " + rTop + " " + rBottom);
 }
 
 function drawRectangle() {
   //render the rectangle and also checks for mouse interactions
   if (mouseX < rRight && mouseX > rLeft && mouseY > rTop && mouseY < rBottom) {
-    fill(220,10,255);  
+    fill(220,10,255);
+    mouseOver = true;  
   }
   else{
     fill(255);
+    mouseOver = false;
+  }
+  if(pickedUp){
+    x = mouseX - xOff;
+    y = mouseY - yOff;   
   }
 
-
   rect(x, y, rWidth, rHeight);
-
 }
 
 function updateEdgePositions() {
@@ -43,5 +48,19 @@ function updateEdgePositions() {
   rRight = x + rWidth / 2;
   rTop = y - rHeight / 2;
   rBottom = y + rHeight / 2;
+}
+
+function mousePressed(){
+  ///triggers on initial mouseDown
+  if(mouseOver){
+    pickedUp = true;
+    xOff = mouseX - x;
+    yOff = mouseY - y;
+  }
+}
+
+function mouseReleased(){
+  //triggers when a pressed mouse button is released
+  pickedUp = false;
 }
 
