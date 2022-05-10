7258f8ad6fbb730131eaadd5a24d5baed7f1755b
stefan-scott <scottst@spsd.sk.ca>
2022-05-10 15:02:42 -0600

    demo in progress

--- 05 Drawing with Loops and CreateGraphics()/sketch.js
+++ 05 Drawing with Loops and CreateGraphics()/sketch.js
@@ -1,19 +1,47 @@
 // Drawing with Loops and createGraphics()
 
 
+let extraCanvas;
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  extraCanvas = createGraphics(width, height);
 }
 
 function draw() {
-  background(220);
+  //background(220);
+  gradientBackground();
   loopCircles();
+  painting();
+
+  //extraCanvas.background(255,0,0);
+  image(extraCanvas, 0,0);
+}
+
+function painting(){
+  extraCanvas.fill(0, 255, 0);
+  extraCanvas.square(mouseX, mouseY, 25);
+}
+
+function gradientBackground(){
+  //create a gradient background using some loops
+  let spacing = 15;
+  noStroke();
+  for(let y = height; y > -spacing; y-= spacing){
+    rect(0,y,width,spacing);
+    let currentColor = map(y, 0, height, 0, 255);
+    fill(0,0,currentColor);  //RGBs
+  }
 }
 
 function loopCircles(){
   //use a loop to draw 0 or more circles in a horizontal line
   let spacing = 40;
   let diameter = 30;
+  fill(255);
+  stroke(0);
+  //interactive:
+  spacing = map(mouseX, 0, width, 10, 50);  
 
   for(let x = width * 0.1; x < width * 0.9; x += spacing){
     circle(x, height/2, diameter);
---
+++ 06 Loops and Arrays/sketch.js
@@ -0,0 +1,57 @@
+// Working with Arrays and loops
+// Mr. Scott
+// May 10, 2022
+// A quick refresher and application of for loops and arrays
+
+//global variables
+let x = [];   //empty arrays
+let y = [];
+const NUM_SEGMENTS = 40;
+const SPEED = 10;
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  initArrays();
+}
+
+function draw() {
+  background(220);
+  updateArrays();
+  drawArrays();
+}
+
+function initArrays(){
+  //populate data into the x and y arrays. (initial values)
+  let x1 = width/2;
+  let y1 = height/2;
+  x.push(x1);
+  y.push(y1);
+
+  for(let i = 0; i < NUM_SEGMENTS-1; i++){
+    x.push(x1 + random(-SPEED, SPEED));
+    y.push(y1 + random(-SPEED, SPEED));
+  }
+}
+
+function drawArrays(){
+  //traverse the parallel arrays, draw some circles and connect them
+  for (let i = 0; i < x.length; i++){
+    fill(0);
+    circle(x[i], y[i], 4);
+
+  }
+}
+
+function updateArrays(){
+  //push one new value to x/y arrays, delete first value
+  //method one: Random
+  x.push(x[x.length-1] + random(-SPEED,SPEED));
+  y.push(y[y.length-1] + random(-SPEED,SPEED));
+
+
+  //remove first element from x/y arrays
+  x.shift();
+  y.shift();
+
+}
