f0669e561f1af20f7057c3defab381e7acc886f7
stefan-scott <scottst@spsd.sk.ca>
2022-05-17 14:10:21 -0600

    finished demo

--- 12 Objects Demo One/sketch.js
+++ 12 Objects Demo One/sketch.js
@@ -2,19 +2,34 @@
 // Mr. Scott   May 17, 2022
 // A first look at Object-Oriented Programming (OOP)
 
-let myWalker;
+//let myWalker;
+const NUM_WALKERS = 110;
+let walkers = [];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   let col = color(random(255) , random(255), random(255));
-  myWalker = new Walker(random(width), random(height), col);
+  //myWalker = new Walker(random(width), random(height), col);
   background(0);
+
+  for(let i = 0; i < NUM_WALKERS ; i++){
+    let col = color(random(255) , random(255), random(255));
+    walkers.push(new Walker(random(width), random(height), col));
+  }
+
 }
 
 function draw() {
   
-  myWalker.move();
-  myWalker.display();
+  //myWalker.move();
+  //myWalker.display();
+
+  //process (move/display all walkers)
+  for(let i = 0; i < walkers.length; i++){
+    walkers[i].move();
+    walkers[i].display();
+  }
+
 
 }
 
@@ -29,6 +44,11 @@ class Walker{
     this.c = c;
     this.speed = 10;
     this.size = 5;
+    this.type = 1;//Math.floor(random(2));  //0 or 1
+    //0 → random walk movement     1 → perlin movement
+    this.timeX = random(999);
+    this.timeY = random(999);
+    this.inc = 0.1;  //how related are subsequent noise() random values
   }
 
   //class methods/functions
@@ -40,7 +60,17 @@ class Walker{
 
   move(){
     //option one - regular random walk
-    this.moveRandom();
+    if(this.type === 0) {this.moveRandom();}
+    else { this.movePerlin(); }
+  }
+
+  movePerlin(){
+    //move using noise()
+    this.x += map(noise(this.timeX),0,1,-this.speed, this.speed);
+    this.y += map(noise(this.timeY),0,1,-this.speed, this.speed);
+
+    this.timeX += this.inc;
+    this.timeY += this.inc;
   }
 
   moveRandom(){
