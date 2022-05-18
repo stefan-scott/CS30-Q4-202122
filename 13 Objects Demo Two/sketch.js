77bf49d7de4c1a66517f91a56ac9028c895e0a1a
stefan-scott <scottst@spsd.sk.ca>
2022-05-18 13:38:15 -0600

    Objects Demo WIP

---
+++ 13 Objects Demo Two/sketch.js
@@ -0,0 +1,72 @@
+// Objects Demo Two
+// Mr. Scott   May 18, 2022
+// A second look at defining our own classes
+// include a look at "enhanced" for loop
+
+let dotCollection = [];
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  initDots();
+}
+
+function initDots(){
+  //use this to generate a whole bunch of DOT objects using While LOOPS
+  let spacing = 12;
+  let currentX = spacing/2;
+  let currentY = spacing/2;
+  while(currentX < width){
+    while(currentY < height){
+      dotCollection.push(new Dot(currentX, currentY, 10));
+      currentY += spacing;
+    }
+    currentY = spacing/2;
+    currentX += spacing;
+  }
+}
+
+function draw() {
+  background(220);
+
+  //loop through each item in the array
+  // for(let i = 0; i < dotCollection.length; i++){
+  //   let d = dotCollection[i];
+  // }
+
+  for(let d of dotCollection){
+    //d temporarily stores each item in collection
+    //can't really handle removals well
+    d.process();
+  }
+
+}
+
+function mousePressed(){
+  dotCollection.push(new Dot(mouseX, mouseY, 10));
+}
+
+
+
+// Class Definition
+class Dot{
+  
+  //class constructor (inc. properties)
+  constructor(x, y, diameter){
+    this.x = x;
+    this.y = y;
+    this.diameter = diameter;
+    this.c = color(255,255,255);
+  }
+
+
+  //class methods
+  display(){
+    fill(this.c);
+    circle(this.x, this.y, this.diameter);
+  }
+
+  process(){
+    //internally call the necessary class methods once per frame
+    this.display();
+  }
+}
