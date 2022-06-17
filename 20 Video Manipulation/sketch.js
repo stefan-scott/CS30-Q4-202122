263c413a224d6d8704ad463a66338f764244977e
stefan-scott <scottst@spsd.sk.ca>
2022-06-17 14:05:57 -0600

    Update sketch.js

--- 20 Video Manipulation/sketch.js
+++ 20 Video Manipulation/sketch.js
@@ -5,13 +5,58 @@
 
 
 let video;
-let GRID_SPACING = 160;
+let GRID_SPACING = 10;
 
 function setup() {
   video = createCapture(VIDEO);
   video.hide();
   createCanvas(640, 480);
+  textSize(GRID_SPACING);
 }
+
+
+function draw(){
+  //used for the ASCII Video Filter Effect
+  video.loadPixels();
+  background(0);
+
+  for(let x = 0; x < video.width; x += GRID_SPACING){
+    for(let y = 0 ; y < video.height; y += GRID_SPACING){
+      let location = (x + y*video.width) * 4; //r position for (x,y) pixel
+      let avg = average(location);
+
+      //Option One: cirlces
+      // fill(avg);
+      // circle(x,y,GRID_SPACING);
+
+      //Option Two: Text
+      drawCharacter(x,y,avg);
+    }
+  }
+}
+
+function drawCharacter(x,y,avg){
+  //inspect the current average intensity and pick a character to 
+  //display at (x,y) to approximate that overall brightness. Drawn on BLACK
+  fill(255);
+  //pick character based on brightness
+  if (avg > 220) text("🍇", x, y);
+  else if(avg > 180) text("🍉", x, y);
+  else if (avg > 130) text("🍌", x, y);
+  else if (avg > 80) text("👀", x, y);
+  else if (avg > 40) text("🎗", x, y);
+
+}
+
+function average(location){
+  //return the average value of r/g/b starting at location
+  let r = video.pixels[location];
+  let g = video.pixels[location+1];
+  let b = video.pixels[location+2];
+  return (r+g+b)/3;
+}
+
+
 
 function multiVideo() {
   //display several videos in a 2D grid
@@ -29,7 +74,23 @@ function setPixelColor(pos, r, g, b) {
   video.pixels[pos + 2] = b;
 }
 
-function draw() {
+function magnify(){
+  //magnify the video feed at the cursor location (on click)
+  if(mouseIsPressed){
+     let cursorRegion = video.get(mouseX-25, mouseY-25, 50, 50);
+     imageMode(CENTER);
+     image(cursorRegion, mouseX, mouseY, cursorRegion.width*3, cursorRegion.height*3);
+     imageMode(CORNER);
+  }
+}
+
+
+
+
+
+
+function originalDraw() {
+  //this was used for greyscale, interlace, magnifier
   background(0);
   video.loadPixels();  //enables access to the pixel array for our video
   //
@@ -44,6 +105,9 @@ function draw() {
       }
     }
   }
+  
   video.updatePixels();
   image(video,0,0);
+  magnify();
+
 }
